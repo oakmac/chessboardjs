0582792de4be5b7a1e767f169e0c0b5b112d1ee6
Chris Oakman <chris@oakmac.com>
2017-06-06 00:11:06 -0500

    docs page

--- build-website.js
+++ build-website.js
@@ -6,6 +6,8 @@ const fs = require('fs')
 const kidif = require('kidif')
 const mustache = require('mustache')
 
+const docs = require('./data/docs.json')
+
 const encoding = {encoding: 'utf8'}
 
 // grab some mustache templates
@@ -79,11 +81,41 @@ function writeExamplesPage () {
   fs.writeFileSync('website/examples.html', html, encoding)
 }
 
+const configTableRowsHTML = docs.config.reduce(function (html, itm) {
+  if (isString(itm)) return html
+  return html + buildPropRowHTML('config', itm)
+}, '')
+
+const methodTableRowsHTML = docs.methods.reduce(function (html, itm) {
+  if (isString(itm)) return html
+  return html + buildMethodRowHTML(itm)
+}, '')
+
+const errorRowsHTML = docs.errors.reduce(function (html, itm) {
+  if (isString(itm)) return html
+  return html + buildErrorRowHTML(itm)
+}, '')
+
+function writeDocsPage () {
+  const headHTML = mustache.render(headTemplate, {pageTitle: 'Documentation'})
+  const headerHTML = mustache.render(headerTemplate, {docsActive: true})
+
+  const html = mustache.render(docsTemplate, {
+    configTableRows: configTableRowsHTML,
+    errorRows: errorRowsHTML,
+    footer: footerTemplate,
+    head: headHTML,
+    header: headerHTML,
+    methodTableRows: methodTableRowsHTML
+  })
+  fs.writeFileSync('website/docs.html', html, encoding)
+}
+
 function writeWebsite () {
   writeSrcFiles()
   writeHomepage()
   writeExamplesPage()
-  // writeDocs()
+  writeDocsPage()
   // writeDownload()
 }
 
@@ -93,10 +125,21 @@ writeWebsite()
 // HTML
 // -----------------------------------------------------------------------------
 
+function htmlEscape (str) {
+  return (str + '')
+           .replace(/&/g, '&amp;')
+           .replace(/</g, '&lt;')
+           .replace(/>/g, '&gt;')
+           .replace(/"/g, '&quot;')
+           .replace(/'/g, '&#39;')
+           .replace(/\//g, '&#x2F;')
+           .replace(/`/g, '&#x60;')
+}
+
 function buildExampleGroupHTML (idx, groupName, examplesInGroup) {
-  const num = idx + 1
-  let html = '<h4 id="groupHeader-' + num + '">' + groupName + '</h4>' +
-    '<ul id="groupContainer-' + num + '" style="display:none">'
+  const groupNum = idx + 1
+  let html = '<h4 id="groupHeader-' + groupNum + '">' + groupName + '</h4>' +
+    '<ul id="groupContainer-' + groupNum + '" style="display:none">'
 
   examplesInGroup.forEach(function (exampleId) {
     const example = examplesObj[exampleId]
@@ -109,7 +152,7 @@ function buildExampleGroupHTML (idx, groupName, examplesInGroup) {
 }
 
 function buildExamplesNavHTML () {
-  var html = ''
+  let html = ''
   examplesGroups.forEach(function (group, idx) {
     html += buildExampleGroupHTML(idx, group.name, group.examples)
   })
@@ -117,7 +160,7 @@ function buildExamplesNavHTML () {
 }
 
 function buildExamplesJS () {
-  var txt = 'window.CHESSBOARD_EXAMPLES = {}\n\n'
+  let txt = 'window.CHESSBOARD_EXAMPLES = {}\n\n'
 
   examplesArr.forEach(function (ex) {
     txt += 'CHESSBOARD_EXAMPLES["' + ex.id + '"] = {\n' +
@@ -132,13 +175,167 @@ function buildExamplesJS () {
   return txt
 }
 
-function htmlEscape (str) {
-  return (str + '')
-           .replace(/&/g, '&amp;')
-           .replace(/</g, '&lt;')
-           .replace(/>/g, '&gt;')
-           .replace(/"/g, '&quot;')
-           .replace(/'/g, '&#39;')
-           .replace(/\//g, '&#x2F;')
-           .replace(/`/g, '&#x60;')
+function buildPropRowHTML (propType, prop) {
+  let html = ''
+
+  // table row
+  html += '<tr id="' + propType + ':' + prop.name + '">'
+
+  // property and type
+  html += '<td>' + buildPropertyAndTypeHTML(propType, prop.name, prop.type) + '</td>'
+
+  // required
+  html += '<td class="center">' + buildRequiredHTML(prop.req) + '</td>'
+
+  // default
+  html += '<td class="center">' + buildDefaultHTML(prop.default) + '</td>'
+
+  // description
+  html += '<td>' + buildDescriptionHTML(prop.desc) + '</td>'
+
+  // examples
+  html += '<td>' + buildExamplesCellHTML(prop.examples) + '</td>'
+
+  html += '</tr>'
+
+  return html
+}
+
+function buildMethodRowHTML (method) {
+  const nameNoParens = method.name.replace(/\(.+$/, '')
+
+  let html = ''
+
+  // table row
+  if (method.noId) {
+    html += '<tr>'
+  } else {
+    html += '<tr id="methods:' + nameNoParens + '">'
+  }
+
+  // name
+  html += '<td><a href="docs.html#methods:' + nameNoParens + '">' +
+    '<code class="js plain">' + method.name + '</code></a></td>'
+
+  // args
+  if (method.args) {
+    html += '<td>'
+    method.args.forEach(function (arg) {
+      html += '<p><code class="js plain">' + arg[0] + '</code> - ' + arg[1] + '</p>'
+    })
+    html += '</td>'
+  } else {
+    html += '<td><small>none</small></td>'
+  }
+
+  // description
+  html += '<td>' + buildDescriptionHTML(method.desc) + '</td>'
+
+  // examples
+  html += '<td>' + buildExamplesCellHTML(method.examples) + '</td>'
+
+  html += '</tr>'
+
+  return html
+}
+
+function buildPropertyAndTypeHTML (section, name, type) {
+  let html = '<p><a href="docs.html#' + section + ':' + name + '">' +
+    '<code class="js plain">' + name + '</code></a></p>' +
+    '<p>' + buildTypeHTML(type) + '</p>'
+  return html
+}
+
+function buildTypeHTML (type) {
+  if (!Array.isArray(type)) {
+    type = [type]
+  }
+
+  let html = ''
+  for (var i = 0; i < type.length; i++) {
+    if (i !== 0) {
+      html += '<small>or</small><br />'
+    }
+    html += type[i]
+  }
+
+  return html
+}
+
+function buildRequiredHTML (req) {
+  if (!req) return 'no'
+  if (req === true) return 'yes'
+  return req
+}
+
+function buildDescriptionHTML (desc) {
+  if (!Array.isArray(desc)) {
+    desc = [desc]
+  }
+
+  let html = ''
+  desc.forEach(function (d) {
+    html += '<p>' + d + '</p>'
+  })
+
+  return html
+}
+
+function buildDefaultHTML (defaultValue) {
+  if (!defaultValue) {
+    return '<small>n/a</small>'
+  }
+  return defaultValue
+}
+
+function buildExamplesCellHTML (examplesIds) {
+  if (!Array.isArray(examplesIds)) {
+    examplesIds = [examplesIds]
+  }
+
+  let html = ''
+  examplesIds.forEach(function (exampleId) {
+    var example = examplesObj[exampleId]
+    if (!example) return
+    html += '<p><a href="examples.html#' + exampleId + '">' + example.name + '</a></p>'
+  })
+
+  return html
+}
+
+function buildErrorRowHTML (error) {
+  let html = ''
+
+  // table row
+  html += '<tr id="errors:' + error.id + '">'
+
+  // id
+  html += '<td class="center">' +
+    '<a href="docs.html#errors:' + error.id + '">' + error.id + '</a></td>'
+
+  // desc
+  html += '<td>' + htmlEscape(error.desc) + '</td>'
+
+  // more information
+  if (error.fix) {
+    if (!Array.isArray(error.fix)) {
+      error.fix = [error.fix]
+    }
+
+    html += '<td>'
+    error.fix.forEach(function (p) {
+      html += '<p>' + p + '</p>'
+    })
+    html += '</td>'
+  } else {
+    html += '<td><small>n/a</small></td>'
+  }
+
+  html += '</tr>'
+
+  return html
+}
+
+function isString (s) {
+  return typeof s === 'string'
 }
